bf6d610a9d99b42992d2818451f95eec273820cc
NikS-afk47 <shepdn06@gmail.com>
2026-04-17 01:47:38 -0500

    main menu

--- stick-fight-game/Scripts/main_menu.gd
+++ stick-fight-game/Scripts/main_menu.gd
@@ -1,11 +1,17 @@
 extends Control
 
+@onready var start_button = $CenterContainer/VBoxContainer/StartButton
+@onready var back_button = $CenterContainer/VBoxContainer/BackButton
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	start_button.pressed.connect(_on_start_pressed)
+	back_button.pressed.connect(_on_back_pressed)
 
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/start_menu.tscn")	
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
